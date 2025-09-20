8b48eb4441ab1a3f6476031d307877cc7d5b5039
GustavoMarizA <100234598+GustavoMarizA@users.noreply.github.com>
2025-09-20 15:40:48 -0300

    fix-Movement
    Correções da movimentação e implementação da NaraMovementController

--- Assets/Logic/Scripts/GameDomain/MVC/Nara/NaraMovementController.cs
+++ Assets/Logic/Scripts/GameDomain/MVC/Nara/NaraMovementController.cs
@@ -1,27 +1,84 @@
 using Logic.Scripts.GameDomain.MVC.Nara;
 using UnityEngine;
+using Logic.Scripts.Turns;
 
-public class NaraMovementController : IMovement {
-    private PlayerData _data;
+public class NaraMovementController : MonoBehaviour, IMovement
+{
     private Rigidbody _rigidbody;
-    public NaraMovementController(PlayerData data, Rigidbody rigidbody) {
-        _data = data;
+    private Vector3 _movement;
+    private Vector3 movementCenter;
+    private int movementRadius;
+    private ActionPointsService _actionPointsService;
+    private int extraMovementSpaceCost = 2;
+
+    public NaraMovementController(NaraConfigurationSO naraSO, Rigidbody rigidbody)
+    {
         _rigidbody = rigidbody;
+        _rigidbody.useGravity = false;
+        _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        _rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+        _rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        movementRadius = naraSO.InitialMovementDistance;
     }
 
-    public void SetNara(NaraView naraView, Vector3 naraPosition) {
+    public void SetNara(NaraView naraView, Vector3 naraPosition)
+    {
 
     }
 
-    public void Jump(float jumpForce, float gravity) {
+    public void Jump(float jumpForce, float gravity)
+    {
         throw new System.NotImplementedException();
     }
 
-    public void Move(Vector2 direction, float velocity, float rotation) {
-        throw new System.NotImplementedException();
+    public void Move(Vector2 direction, float velocity, float rotation)
+    {
+        float distance = Vector3.Distance(transform.position, movementCenter);
+
+        if(distance < movementRadius)
+        {
+            if (distance >= movementRadius / 2)
+            {
+                _actionPointsService.Spend(extraMovementSpaceCost);
+            }
+            Vector3 _direction = new Vector3(direction.x, 0f, direction.y);
+            _movement = _direction * velocity;
+            _rigidbody.linearVelocity = new Vector3(_movement.x, 0f, _movement.z);
+            Rotate(rotation);
+        }
     }
 
-    public void MoveToPoint(Vector3 endPosition, float velocity, float rotation) {
-        throw new System.NotImplementedException();
+    public void MoveToPoint(Vector3 endPosition, float velocity, float rotation)
+    {
+        float distance = Vector3.Distance(endPosition, movementCenter);
+
+        if(distance < movementRadius)
+        {
+            if(distance >= movementRadius/2)
+            {
+                _actionPointsService.Spend(extraMovementSpaceCost);
+            }
+            Vector3 direction = endPosition - transform.position;
+            direction.y = 0f;
+            Vector3 n = direction.magnitude > 0.0001f ? direction.normalized : Vector3.zero;
+            _movement = n * velocity;
+            _rigidbody.linearVelocity = new Vector3(_movement.x, 0f, _movement.z);
+            Rotate(rotation);
+        }
+    }
+
+    private void Rotate(float rotationForce)
+    {
+        Vector3 rotate = new Vector3(_rigidbody.linearVelocity.x, 0f, _rigidbody.linearVelocity.z);
+        if (rotate.sqrMagnitude > 0.0001f)
+        {
+            Quaternion finalRotation = Quaternion.LookRotation(rot.normalized, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, finalRotation, Time.fixedDeltaTime * rotationForce);
+        }
+    }
+
+    public void SetMovementRadiusCenter()
+    {
+        movementCenter = transform.position;
     }
 }

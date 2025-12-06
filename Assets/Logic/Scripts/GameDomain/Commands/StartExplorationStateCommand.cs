829945a6f8d090ade2251bbb73c5c29f69d245ae
GustavoMarizA <gustavo.mariz.andrade@gmail.com>
2025-12-06 17:06:16 -0300

    fix-NaraSFX

--- Assets/Logic/Scripts/GameDomain/Commands/StartExplorationStateCommand.cs
+++ Assets/Logic/Scripts/GameDomain/Commands/StartExplorationStateCommand.cs
@@ -24,11 +24,13 @@ public class StartExplorationStateCommand : BaseCommand, ICommandAsync {
         _naraController = _diContainer.Resolve<INaraController>();
         _commandFactory = _diContainer.Resolve<ICommandFactory>();
         _gameInputActionsController = _diContainer.Resolve<IGameInputActionsController>();
+        _audioService = _diContainer.Resolve<IAudioService>();
     }
 
     public async Awaitable Execute(CancellationTokenSource cancellationTokenSource) {
         _gameInputActionsController.RegisterExplorationInputListeners();
         await _commandFactory.CreateCommandAsync<StartLevelCommand>().Execute(cancellationTokenSource);
         _naraController.InitEntryPointExploration();
+        _audioService.PlayAudio(AudioClipType.LobbyTheme, AudioChannelType.Music, AudioPlayType.Loop);
     }
 }
--- Assets/Logic/Scripts/GameDomain/MVC/Cast/CastController.cs
+++ Assets/Logic/Scripts/GameDomain/MVC/Cast/CastController.cs
@@ -5,83 +5,120 @@ using Logic.Scripts.Services.CommandFactory;
 using Logic.Scripts.Services.UpdateService;
 using Logic.Scripts.Turns;
 using UnityEngine;
+using Zenject;
+using Logic.Scripts.Services.AudioService;
 
-public class CastController : ICastController {
+public class CastController : ICastController
+{
     private readonly IActionPointsService _actionPointsService;
     private readonly IUpdateSubscriptionService _subscriptionService;
     private readonly ICommandFactory _commandFactory;
+    private readonly AbilityData[] _abilities;
 
     private AbilityData _currentAbility;
+    private IEffectable _currentCaster;
+    private bool _canUseAbility;
+    private int _currentAbilityIndex = -1;
 
-    private readonly AbilityData[] _abilities;
     public Transform PlayerTransform;
 
-    private bool _canUseAbility;
-    private IEffectable _currentCaster;
+    private IAudioService _audio;
 
     public CastController(IUpdateSubscriptionService updateSubscriptionService, ICommandFactory commandFactory,
-        IActionPointsService actionPointsService, AbilityData[] abilities) {
+        IActionPointsService actionPointsService, AbilityData[] abilities)
+    {
         _subscriptionService = updateSubscriptionService;
         _actionPointsService = actionPointsService;
         _commandFactory = commandFactory;
         _abilities = abilities;
-    }
-    public void InitEntryPoint(INaraController naraController) {
-        PlayerTransform = naraController.NaraViewGO.transform;
-        foreach (AbilityData ability in _abilities) {
-            ability.SetUp(_subscriptionService, _commandFactory);
-        }
+
+        try { _audio = ProjectContext.Instance.Container.Resolve<IAudioService>(); } catch { _audio = null; }
     }
 
-    public bool TryUseAbility(int index, IEffectable caster) {
-        if (_actionPointsService.CanSpend(_abilities[index].GetCost())) {
+    public void InitEntryPoint(INaraController naraController)
+    {
+        PlayerTransform = naraController.NaraViewGO.transform;
+        foreach (AbilityData ability in _abilities)
+            ability.SetUp(_subscriptionService, _commandFactory);
+    }
+
+    public bool TryUseAbility(int index, IEffectable caster)
+    {
+        if (_actionPointsService.CanSpend(_abilities[index].GetCost()))
+        {
             _abilities[index].Aim(caster);
             _currentAbility = _abilities[index];
             _currentCaster = caster;
-            if (caster is INaraController naraController) {
+            _currentAbilityIndex = index;
+
+            if (caster is INaraController naraController)
+            {
                 int attackType = _abilities[index] != null ? _abilities[index].AnimatorAttackType : 1;
                 naraController.PlayAttackType(attackType);
             }
             return true;
         }
-        else {
+        else
+        {
             return false;
         }
     }
 
-    public void CancelAbilityUse() {
-        if (_currentCaster is INaraController naraController) {
+    public void CancelAbilityUse()
+    {
+        if (_currentCaster is INaraController naraController)
             naraController.TriggerCancel();
-        }
+
         _currentAbility?.Cancel();
         _currentAbility = null;
         _currentCaster = null;
+        _currentAbilityIndex = -1;
     }
 
-    public void UseAbility(IEffectable caster) {
+    public void UseAbility(IEffectable caster)
+    {
         if (_currentAbility == null) return;
+
         _canUseAbility = true;
         _actionPointsService.Spend(_currentAbility.GetCost());
-        if (caster is INaraController naraController) {
+
+        if (caster is INaraController naraController)
+        {
             naraController.TriggerExecute();
         }
+
+        PlayUsedSfxByIndex(_currentAbilityIndex);
+
         _currentAbility.Cast(caster);
         CancelAbilityUse();
     }
 
-    public bool GetCanUseAbility() {
-        return _canUseAbility;
-    }
+    public bool GetCanUseAbility() => _canUseAbility;
+    public void SetCanUseAbility(bool b) => _canUseAbility = b;
 
-    public void SetCanUseAbility(bool b) {
-        _canUseAbility = b;
-    }
-
-    public void UseFastEcho(IEchoController echoController, Transform caster) {
+    public void UseFastEcho(IEchoController echoController, Transform caster) =>
         echoController.CreateFastEcho(caster);
+
+    public void UseSlowEcho(IEchoController echoController, Transform caster) =>
+        echoController.CreateSlowEcho(caster);
+
+    private void PlayUsedSfxByIndex(int index)
+    {
+        if (_audio == null) return;
+
+        AudioClipType clip = MapUsedClip(index);
+        _audio.PlayAudio(clip, AudioChannelType.Fx, AudioPlayType.OneShot);
     }
 
-    public void UseSlowEcho(IEchoController echoController, Transform caster) {
-        echoController.CreateSlowEcho(caster);
+    private static AudioClipType MapUsedClip(int index)
+    {
+        switch (index)
+        {
+            case 0: return AudioClipType.AbilityUsed1SFX;
+            case 1: return AudioClipType.AbilityUsed2SFX;
+            case 2: return AudioClipType.AbilityUsed3SFX;
+            case 3: return AudioClipType.AbilityUsed4SFX;
+            default: return AudioClipType.AbilityUsed5SFX;
+        }
     }
 }
